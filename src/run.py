24281e6c80039a53dc1d2424d907568d505e43d8
Emil Huseynaliev <emilh1791@gmail.com>
2015-12-19 18:16:40 -0500

    Outputs status and checking against validation and testing set

--- src/run.py
+++ src/run.py
@@ -1,18 +1,27 @@
-import mnist_loader
+import mnist_loader as ml
 import network
-"""
-Load data, set them into different sets:
-training data, validation data and test data
-"""
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+import numpy as np
 
-""" 
-Initial the training network, specify the node number of input/hidden/output
-"""
-net = network.Network([784,30,10])
+def arctan_prime(z):
+    return 1.0 / (1 + z**2)
+    
+def main():
+    print "Loading MNIST data..."
+    training_data, validation_data, test_data = ml.load_data_wrapper()
 
-"""
-Trigger the training/test process
-Need to provice the epoch of training, mini-batch size and learning rate
-"""
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+    net = network.Network([784,30,10])
+    net.set_random_weights_and_biases()
+    #net.set_transfer_function(np.arctan)
+    #net.set_transfer_derivative(arctan_prime)
+    
+    epochs = 30
+    mbs = 10 # mini-batch size
+    eta = 3.0 # learning rate
+    print "Training net: epochs={}, MBS={}, eta={}...".format(epochs, mbs, eta)
+    net.SGD(training_data, epochs, mbs, eta)
+
+    print "{} validation samples correctly classified".format(net.evaluate(validation_data))
+    print "{} testing samples correctly classified".format(net.evaluate(test_data))
+
+if __name__ == '__main__':
+    main()
