b744c6552296c65f9561f20eb071549681dc3bb8
Tao Kang <kangkang@172-16-183-103.DYNAPOOL.NYU.EDU>
2015-12-19 20:21:00 -0500

    Add running time analysis for run.py

--- src/run.py
+++ src/run.py
@@ -1,5 +1,6 @@
 import mnist_loader
 import network
+import time
 """
 Load data, set them into different sets:
 training data, validation data and test data
@@ -15,4 +16,8 @@ net = network.Network([784,30,10])
 Trigger the training/test process
 Need to provice the epoch of training, mini-batch size and learning rate
 """
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+start_time = time.time();
+
+net.SGD(training_data, 30, 10, 0.3, test_data=test_data)
+
+print "------it takes %d seconds to complete the whole peocess-----" %(time.time() - start_time)
